a3cff5bf30ba73d65f7da48c9838d1de650519aa
Clara de Alcântara <161838054+oiclai@users.noreply.github.com>
2024-11-08 01:58:31 -0300

    :books: docs: add files

---
+++ beecrowd-iniciante/1001.cs
@@ -0,0 +1,15 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) 
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            
+            int n2 = int.Parse(Console.ReadLine());
+            
+            int soma = n1 + n2;
+            
+            Console.WriteLine("X = " + soma);
+        }
+}
