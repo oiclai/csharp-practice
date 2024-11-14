cbcf592ea455ce51a50c6743a5d24880e12abdf1
Clara de Alcântara <161838054+oiclai@users.noreply.github.com>
2024-11-14 01:34:11 -0300

    :books: add file

---
+++ beecrowd-iniciante/1012.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+
+        string[] valores = Console.ReadLine().Split(' ');
+        double A = double.Parse(valores[0]);
+        double B = double.Parse(valores[1]);
+        double C = double.Parse(valores[2]);
+
+        const double pi = 3.14159;
+
+        double areaTriangulo = (A * C) / 2.0;
+        double areaCirculo = pi * Math.Pow(C, 2);
+        double areaTrapezio = ((A + B) * C) / 2.0;
+        double areaQuadrado = Math.Pow(B, 2);
+        double areaRetangulo = A * B;
+
+        Console.WriteLine("TRIANGULO: " + areaTriangulo.ToString("F3"));
+        Console.WriteLine("CIRCULO: " + areaCirculo.ToString("F3"));
+        Console.WriteLine("TRAPEZIO: " + areaTrapezio.ToString("F3"));
+        Console.WriteLine("QUADRADO: " + areaQuadrado.ToString("F3"));
+        Console.WriteLine("RETANGULO: " + areaRetangulo.ToString("F3"));
+    }
+}
