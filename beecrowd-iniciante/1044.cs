86919df9f2cf7ed967ccb488a775bc22c6eb9b43
Clara de Alcântara <161838054+oiclai@users.noreply.github.com>
2024-11-27 16:54:36 -0300

    :sparkles: feat: add features

--- beecrowd-iniciante/1044.cs
+++ beecrowd-iniciante/1044.cs
@@ -1,15 +1,22 @@
 using System; 
 
 class URI {
-    
-    public static i
-    
+    public static string SaoMultiplos (int a, int b){
+        if ((a%b == 0) || (b%a ==0)){
+            return "Sao Multiplos";
+        }
+        else{
+            return "Nao sao Multiplos";
+        }
+    }
     static void Main(string[] args) { 
-
-        string [] valores = Console.ReadLine().Split(' ');
+        string [] valores = Console.ReadLine().Split();
         int a = int.Parse(valores[0]);
         int b = int.Parse(valores[1]);
-        var resultado = 
+        var resultado = SaoMultiplos(a, b);
+        Console.WriteLine(resultado);
+        
+
     }
 
 }
