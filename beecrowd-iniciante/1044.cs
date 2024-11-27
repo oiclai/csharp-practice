5085bf43cccae1527dd28bdf2ef0c862ea770513
Clara de Alcântara <161838054+oiclai@users.noreply.github.com>
2024-11-27 16:12:35 -0300

    :construction: in progress ...

---
+++ beecrowd-iniciante/1044.cs
@@ -0,0 +1,15 @@
+using System; 
+
+class URI {
+    
+    public static i
+    
+    static void Main(string[] args) { 
+
+        string [] valores = Console.ReadLine().Split(' ');
+        int a = int.Parse(valores[0]);
+        int b = int.Parse(valores[1]);
+        var resultado = 
+    }
+
+}
