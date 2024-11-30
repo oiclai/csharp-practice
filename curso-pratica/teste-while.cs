4400c4082d4b3d1afdad9b809e1a198acb323aae
oiclai <claradealcantara285@gmail.com>
2024-11-30 14:07:17 -0300

    :truck: chore: rename file

---
+++ curso-pratica/teste-while.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Globalization;
+
+namespace Course {
+    class Program {
+        static void Main (string[] args){
+            Console.Write("Digite um número: ");
+            double num = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            while (num >= 0.0){
+                double raiz = Math.Sqrt(num);
+                Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+                Console.Write("Digite um número: ");
+                double num = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            Console.WriteLine("Número negativo.");
+        }
+    }    
+}
--- obj/Debug/net8.0/csharp-practice.AssemblyInfo.cs
+++ obj/Debug/net8.0/csharp-practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp-practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86919df9f2cf7ed967ccb488a775bc22c6eb9b43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cffd8fcf6d439ae719a2e5f03cf4ab5cd50e34ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp-practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp-practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
