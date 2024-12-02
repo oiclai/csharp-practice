6a33ea254a673c5dd019bd8ef4135591d2fc85f8
oiclai <claradealcantara285@gmail.com>
2024-12-02 10:25:37 -0300

    :books: docs: add file

--- obj/Debug/net8.0/csharp-practice.AssemblyInfo.cs
+++ obj/Debug/net8.0/csharp-practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp-practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+641dde61c752a8c2ec2483f3ca0ddb2f9d1c778a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e35ce4862439326576be677d9e39ea7fa6b4a98f")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp-practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp-practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ pratica-poo/student.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Globalization;
+namespace Course {
+  class Aluno {
+    public string Nome;
+    public double Nota1, Nota2, Nota3;
+    public double Imposto;
+
+    public double NotaFinal() {
+      return Nota1 + Nota2 + Nota3;
+    }
+
+    public AprovadoOuReprovado(double notafinal) {
+      if (notafinal < 60.0) {
+        return "REPROVADO\nFALTARAM " + (60.0 - notafinal) + " PONTOS";
+      }
+      else {
+        return "APROVADO";
+      }
+    }
+  }
+  class Programa {
+    static void Main (string[] args) {
+        Aluno a = new Aluno();
+        Console.Write("Nome: ");
+        a.Nome = Console.ReadLine();
+        Console.WriteLine("");
+        Console.Write("1ª Nota: ");
+        a.Nota1 = Console.ReadLine();
+        Console.WriteLine("");
+        Console.Write("2ª Nota: ");
+        a.Nota2 = Console.ReadLine();
+        Console.WriteLine("");
+        Console.Write("3ª Nota: ");
+        a.Nota3 = Console.ReadLine();
+        Console.WriteLine("");
+        Console.WriteLine("NOTA FINAL = " + a.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine(a.AprovadoOuReprovado(a.NotaFinal()));
+    }
+  }
+}
