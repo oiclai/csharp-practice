49995f3f23452ea169151b5a5afa9aa89660c3d7
Clara de Alcântara <161838054+oiclai@users.noreply.github.com>
2024-12-02 01:12:17 -0300

    :lipstick: style: style writeline

--- pratica-poo/product.cs
+++ pratica-poo/product.cs
@@ -10,6 +10,15 @@ namespace Course {
     public double ValorTotalEmEstoque() {
       return Preco * Quantidade;
     }
+    public override string ToString(){
+      return Nome
+        + ", $ "
+        + Preco.ToString("F2", CultureInfo.InvariantCulture)
+        + ", "
+        + Quantidade
+        + " unidades, Total: $ "
+        + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+    }
   }
   class Programa {
     static void Main (string[] args) {
@@ -22,7 +31,7 @@ namespace Course {
       Console.Write ("Quantidade: ");
       p.Quantidade = int.Parse(Console.ReadLine());
 
-      Console.WriteLine("Dados do Produto: " );
+      Console.WriteLine("Dados do Produto: " +p);
     }
   }
 }
